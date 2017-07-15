3dcdb1d0b3b540b6808772854a71d9cd3fca643b
kousik <kousik@cisco.com>
2017-07-15 21:37:00 +0530

    ex8 unregularized version just enough to pass the course

--- ex8/machine-learning-ex8/ex8/cofiCostFunc.m
+++ ex8/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,18 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% for i = 1:num_movies,
+%   for j = 1:num_users,
+%     if R(i,j) == 1,
+%       J += (X(i,:)*Theta(j,:)' - Y(i,j))^2;
+%     end
+%   end
+% end
+% J/=2;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = sum(sumsq((X*Theta' - Y).*R)/2);
+X_grad = ((X*Theta' - Y).*R)*Theta;
+Theta_grad = ((X*Theta' - Y).*R)'*X;
 
 % =============================================================
 
